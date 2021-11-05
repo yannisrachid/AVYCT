b48288bd2cd350e851989f0786b0f4a8940c0103
vmelancon <victor.melancon0@gmail.com>
2021-11-05 10:34:11 +0100

    index almost done, rdf missing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require('express');
+const fs = require("fs");
 
 const CollectorMega = require("./collectorMega");
 
@@ -12,7 +13,7 @@ const collect = (res, path, type, query) => {
     var collector = new CollectorMega(path, type, query);
     // console.log(collector);
     const data = collector.compute();
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (e) {
     return {error : "Too bad : " + e.toString()};
@@ -22,29 +23,44 @@ const collect = (res, path, type, query) => {
 app.get("/", function (req, res) {
   console.log(req.query)
   res.format({
-    json : () => {
+    "application/json" : () => {
       console.log("JSON");
       const data = collect(res, "all", "json", req.query);
       res.status(("error" in data) ? 400 : 200);
       res.json(data);
     },
 
-    xml : () => {
+    "application/xml" : () => {
+      console.log("XML");
       const data = collect(res, "all", "xml", req.query);
-      res.status(("error" in data) ? 400 : 200);
+      res.status((typeof data !== "string") ? 400 : 200);
       res.send(data);
     },
 
-    rdf : () => {
+    "application/rdf+xml" : () => {
+      console.log("RDF/XML");
       const data = collect(res, "all", "rdf", req.query);
-      res.status(("error" in data) ? 400 : 200);
-      res.write(data);
+      res.status((typeof data !== "string") ? 400 : 200);
+      res.send(data);
     },
   
     default : () => {
       res.status(406).send('Not Acceptable');
     }
   });
+
+});
+
+app.get("/rdf", function (req, res) {
+  try {
+    const rdf = fs.readFileSync("my_rdf.rdf", "utf-8");
+    res.status(200);
+    res.send(rdf);  
+  } catch (e) {
+    res.status(400);
+    res.send("Error during file parse.");
+  }
+});
 
   // var collector = new CollectorMega("all", "json", req.query);
   // console.log(collector);
@@ -53,7 +69,6 @@ app.get("/", function (req, res) {
   // res.status(("error" in data) ? 400 : 200);
   // res.send(data);
 
-});
 
 app.listen(PORT, function () {
   console.log('Serveur sur le port :' + PORT);
